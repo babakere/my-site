10244d0edc37c5ca0b354f1358a42c6bb44cb95d
intel <evanbabaker@hotmail.com>
2026-03-09 08:47:38 +0000

    setting up basic website

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        &copy; {new Date().getFullYear()} Evan Barbers. All rights reserved.
+      </p>
+    </footer>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import {Menu, X} from "@phosphor-icons/react";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav>
+
+        <div>
+
+        </div>
+    </nav>
+    );
+}
